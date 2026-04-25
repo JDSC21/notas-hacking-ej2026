029b91b41012f22edf3b5db74345b806bf99f426
David Sanchez <jesdavsc@hotmail.com>
2026-04-25 15:13:39 -0600

    Moví carpeta

---
+++ picoctf/crypto3/mindPsandQs/mindPsQs.py
@@ -0,0 +1,19 @@
+c = 15341890103764929939105506004034128738090325640037083301857608662849501626260517
+n = 948406957756830799684818171639547165784816468744946013083947881743680617123566349
+e = 65537
+
+# se obtienen de la factorizacion en factordb.com
+p = 1891771437429478964908181306574287207137
+q = 501332739776173570344039681219489434626477
+
+tn = (p-1) * (q-1)
+ 
+d = pow(e, -1, tn)
+m = pow(c,d,n)
+
+print(f'm = {m}')
+flag = bytes.fromhex('0' + hex(m)[2:]).decode()
+print(f'flag = {flag}')
+#invertir cadena en python
+flag = flag[::-1]
+print(f'flag invertida = {flag}')
